c273d65e29010fd5ef5570bfdf3f413b4db5e6ba
Lucciola <hidenari.t1997nov@gmail.com>
2018-10-21 15:14:37 +0900

    fix: draggable frame layout

--- gesturepack/src/main/java/ml/clockvoid/gesturepack/HorizontalDraggableFrameLayout.kt
+++ gesturepack/src/main/java/ml/clockvoid/gesturepack/HorizontalDraggableFrameLayout.kt
@@ -1,53 +1,16 @@
 package ml.clockvoid.gesturepack
 
 import android.content.Context
-import android.content.res.TypedArray
-import android.support.v4.view.NestedScrollingParent
 import android.util.AttributeSet
-import android.util.Log
-import android.view.View
-import android.widget.FrameLayout
 
-class HorizontalDraggableFrameLayout : FrameLayout, NestedScrollingParent {
+class HorizontalDraggableFrameLayout : DraggableFrameLayout {
 
     constructor(context: Context) : this(context, null, 0)
 
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        delegate = HorizontalDelegate(this)
         init(context, attrs)
-    }
-
-    private var delegate = HorizontalDelegate(this)
-
-    private fun init(context: Context, attrs: AttributeSet?) {
-        Log.d("init", "init called")
-        val a: TypedArray = getContext().obtainStyledAttributes(attrs, R.styleable.VerticalDraggableFrameLayout, 0, 0)
-        delegate.init(context, a)
-        a.recycle()
-    }
-
-    override fun onStartNestedScroll(child: View, target: View, nestedScrollAxes: Int): Boolean {
-        Log.d("startscroll", "started")
-        return delegate.onStartNestedScroll(child, target, nestedScrollAxes)
-    }
-
-    override fun onNestedPreScroll(target: View, dx: Int, dy: Int, consumed: IntArray) {
-        Log.d("prescroll", "scrolling")
-        delegate.onNestedPreScroll(target, dx, dy, consumed)
-    }
-
-    override fun onNestedScroll(target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int) {
-        Log.d("scroll", "scrolling")
-        delegate.onNestedScroll(target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed)
-    }
-
-    override fun onStopNestedScroll(p0: View) {
-        Log.d("stopscroll", "stopscrolling")
-        delegate.onStopNestedScroll(p0)
-    }
-
-    fun addListener(callback: Callback) {
-        delegate.addListener(callback)
     }
 }
